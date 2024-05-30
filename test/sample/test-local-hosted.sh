229366f68c068709e1b69efc105f5d2669de8c6d
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-05-30 08:14:15 +0700

    test(local-hosted): double the timeout waiting for service startup

--- test/sample/test-local-hosted.sh
+++ test/sample/test-local-hosted.sh
@@ -21,8 +21,8 @@ echo '# Build and run Extend app locally'
 (cd gateway && BASE_PATH=/$APP_BASE_PATH go run main.go) & GATEWAY_PID=$!
 (cd src/AccelByte.Extend.ServiceExtension.Server && dotnet run) & SERVICE_PID=$!
 
-(for _ in {1..12}; do curl http://localhost:8000 >/dev/null 2>&1 && exit 0 || sleep 5s; done; exit 1)
-(for _ in {1..12}; do curl http://localhost:8080 >/dev/null 2>&1 && exit 0 || sleep 5s; done; exit 1)
+(for _ in {1..12}; do curl http://localhost:8000 >/dev/null 2>&1 && exit 0 || sleep 10s; done; exit 1)
+(for _ in {1..12}; do curl http://localhost:8080 >/dev/null 2>&1 && exit 0 || sleep 10s; done; exit 1)
 
 if [ $? -ne 0 ]; then
   echo "Failed to run Extend app locally"
