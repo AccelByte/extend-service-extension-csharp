7a3a4302029df4502f42d8f49abad5d4004aa89d
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-03-17 13:36:14 +0700

    feat(wrapper.sh): forward sigterm

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-./AccelByte.Extend.ServiceExtension.Server &
+kill_services()
+{
+    if [ "$SERVER_PID" ] && [ "$GATEWAY_PID" ]; then
+        kill -TERM $SERVER_PID $GATEWAY_PID 2>/dev/null
+    else
+        KILL_SERVICES_ONCE_STARTED="yes"
+    fi
+}
 
-./grpc-gateway &
+trap 'kill_services' TERM INT
 
-wait -n
+./AccelByte.Extend.ServiceExtension.Server & SERVER_PID=$!
+./grpc-gateway & GATEWAY_PID=$!
 
-exit $?
+if [ "$KILL_SERVICES_ONCE_STARTED" ]; then
+    kill_services
+fi
 
-# https://docs.docker.com/config/containers/multi-service_container/#use-a-wrapper-script
+while true; do
+    if kill -0 $SERVER_PID $GATEWAY_PID 2>/dev/null; then
+        sleep 1s
+    else
+        kill_services
+        break
+    fi
+done
+
+wait $SERVER_PID
+SERVER_EXIT_CODE=$?
+
+wait $GATEWAY_PID
+GATEWAY_EXIT_CODE=$?
+
+echo Server exit code: $SERVER_EXIT_CODE
+echo Gateway exit code: $GATEWAY_EXIT_CODE
+
+if [ $SERVER_EXIT_CODE -gt 0 ]; then
+    exit $SERVER_EXIT_CODE
+else
+    exit $GATEWAY_EXIT_CODE
+fi
